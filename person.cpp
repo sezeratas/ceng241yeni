3564f8b906850da1fb0f1c4018fd899b4b8d9a57
berk.memis <berk.memis@sono.com.tr>
2023-12-30 22:42:17 +0300

    kalıtım sınıfları eklendi

--- person.cpp
+++ person.cpp
@@ -1,26 +1,10 @@
+#include "person.h"
 #include "Classes.h"
 #include <string>
 #include <vector>
 
 using namespace std;
 
-// Adds
-Add::Add() {
-	int addId;
-}
-Add::Add(int addId, string addDate) {
-	this->addId = addId;
-	this->addDate = addDate;
-}
-
-Add::~Add() {
-	cout << "Add deleted." << endl << endl;
-}
-
-// Pet
-
-
-//  Person
 Person::Person() {
 	name = "-Not known-";
 	surname = "-Not known-";
@@ -38,37 +22,18 @@ Person::~Person() {
 	cout << "Person deleted." << endl << endl;
 }
 
-// Adds
-int Add::getAddId() {
-	return NULL;
-}
-void Add::setAddId(int id) {
-
-}
-
-string Add::getAddDate() {
-	return NULL;
-}
-void Add::setAddDate(string addDate) {
-
-}
-
-//  Pets
-
-
-//  Person
 string Person::getName() {
 	return NULL;
 }
 void  Person::setName(string name) {
-	
+
 }
 
 string Person::getSurname() {
 	return NULL;
 }
 void  Person::setSurname(string surname) {
-	
+
 }
 
 string Person::getPhoneNumber() {
